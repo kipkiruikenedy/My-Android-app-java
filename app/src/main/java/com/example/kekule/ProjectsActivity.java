07a64e49cfebc05cfac8847bd5cdcb04b9b01c05
kenedy kipkirui <kipkiruikenedy@gmail.com>
2023-02-07 19:50:54 +0300

    first commit

---
+++ app/src/main/java/com/example/kekule/ProjectsActivity.java
@@ -0,0 +1,14 @@
+package com.example.kekule;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProjectsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_projects);
+    }
+}
